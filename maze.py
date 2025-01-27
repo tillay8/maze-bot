7f8c2e7755aa481521f180ccb096973b4a8cbdef
Tilley <tilleyy8@gmail.com>
2025-01-27 09:16:25 -0700

    Update maze.py

--- maze.py
+++ maze.py
@@ -2,6 +2,7 @@ import random, time
 from PIL import Image
 
 Scale = 10
+#SIDE, START, PATH, END = (0, 228, 48), (255, 0, 0), (0, 121, 241), (0, 0, 0)
 SIDE, START, PATH, END = (0, 0, 0), (255, 0, 0), (255, 255, 255), (0, 0, 255)
 
 def export(num):
@@ -44,18 +45,21 @@ def fill(field, o, n, W):
         field[((n[1] - o[1]) // 2 + o[1]) * W + o[0]] = 1
 
 def createImage(file, field, W, H):
-    img = Image.new("RGB", ((W+2) * Scale, (H+2) * Scale), SIDE)
+    img = Image.new("RGB", (W * Scale, H * Scale), PATH)
     pixels = img.load()
     for x in range(W):
         for y in range(H):
             c = SIDE if field[x + y * W] == 0 else PATH
             for i in range(Scale):
                 for j in range(Scale):
-                    pixels[(x+1) * Scale + i, (y+1) * Scale + j] = c
+                    pixels[x * Scale + i, y * Scale + j] = c
     for i in range(Scale):
         for j in range(Scale):
-            pixels[i+Scale, j+Scale] = START
+            pixels[i, j] = START
     for i in range(Scale):
         for j in range(Scale):
-            pixels[(W) * Scale + i, (H) * Scale + j] = END
+            pixels[(W - 1) * Scale + i, (H - 1) * Scale + j] = END
+    img.save(file)
     return file
+
+export(127)
