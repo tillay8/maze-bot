09b916a9e53c9e280096474ae489c5aaf947025a
Tilley <tilleyy8@gmail.com>
2025-02-01 20:58:05 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 gcc -o maze maze.c -lpng
 if [ "$1" = "-n" ]; then
     nohup python3 mazebot.py &
-elif ["$1" ="-k" ]; then
+elif [ "$1" ="-k" ]; then
     pkill -f "python3 mazebot.py"
 else
     python3 mazebot.py
