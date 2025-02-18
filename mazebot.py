dddcd091eeb50d4daea35ae99b72b28c2106124d
Tilley <tilleyy8@gmail.com>
2025-02-18 05:34:34 -0700

    rewrite logging

--- mazebot.py
+++ mazebot.py
@@ -1,101 +1,86 @@
-import discord
-import subprocess
-import os
-import time
+import discord, subprocess, os, time
+from datetime import datetime
 from discord.ext import commands
 
-# Read bot token from file
 with open(os.path.expanduser("~/bot_tokens/SlashMaze_token"), 'r') as f:
     token = f.readline().strip()
 
-# Bot setup
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-wait_message = "matte kudasai! >.<"
+output_file = "maze.png"
+
+async def try_to_send(operation, message=None, file=None):
+    if isinstance(operation, discord.Interaction):
+        if not operation.response.is_done():
+            if file:
+                await operation.response.send_message(file=discord.File(file))
+            else:
+                await operation.response.send_message(message)
+        else:
+            if file:
+                await operation.followup.send(file=discord.File(file))
+            else:
+                await operation.followup.send(message)
+    else:
+        if file:
+            await operation.reply(file=discord.File(file))
+        else:
+            await operation.reply(message)
+
 def separate(input_string):
     return input_string.split()
 
+def log(message):
+    with open("./mazebot.log", "a") as f:
+        f.write(f"\n{message}")
+
 async def generate_maze(operation, size_input=None):
-    if not size_input or not size_input[0].isnumeric():
-        message = "Please enter an integer for size!"
-        # Send the initial response to the interaction
-        if isinstance(operation, discord.Interaction):
-            # Ensure we only send one response
-            if not operation.response.is_done():
-                await operation.response.send_message(message)
-        else:
-            await operation.channel.send(message)
-        print("\033[31mUser did not input an integer for size\033[0m")
+    if not size_input or not size_input[0].lstrip('f').isnumeric():
+        await try_to_send(operation, "Please enter an integer for size!")
+        log(f"\033[31mUser did not input an integer for size: \033[34m{size_input}\033[0m")
         return
 
-    num = int(size_input[0])
+    force = size_input[0].startswith('f')
+    if force: log("\033[32mUser is forcing maze generation\033[0m")
+    num = int(size_input[0].lstrip('f'))
     num = num if num % 2 != 0 else num + 1
 
-    output_file = "maze.png"
-    try:
-        # Only send the wait message if the maze width is greater than 512
-        if isinstance(operation, discord.Interaction) and num > 800 and not operation.response.is_done():
-            await operation.response.send_message(wait_message)
-            print("\033[33mSent wait message.\033[0m")
-
-        start_time = time.time()
-        subprocess.run(["./maze", str(num), output_file], check=True)
-        generation_time = time.time() - start_time
-
-        maze_size = os.path.getsize(output_file) / (1024 * 1024)  # Size in MB
-        print(f"Maze size: \033[31m{maze_size:.2f}\033[0m MB" if maze_size > 10 else f"Maze size: \033[92m{maze_size:.2f}\033[0m MB")
-        print(f"Time to generate: \033[95m{generation_time:.2f}\033[0m seconds")
-
-        if maze_size > 10:
-            size_warning = f"Maze was generated successfully but is too large to send ({round(maze_size)} MB)"
-            if isinstance(operation, discord.Interaction):
-                # If the interaction was already responded to, we can only use followup
-                if not operation.response.is_done():
-                    await operation.response.send_message(size_warning)
+    if force or num <= 7800:
+        try:
+            if isinstance(operation, discord.Interaction) and num > 800 and not operation.response.is_done():
+                if force:
+                    await try_to_send(operation, "You're bypassing the filesize limit. If you cause an error thats ur fault")
                 else:
-                    await operation.followup.send(size_warning)
-            else:
-                await operation.reply(size_warning)
-            return
+                    await try_to_send(operation, "You put in big maze size. It will take a bit")
+                log("\033[33mSent wait message.\033[0m")
 
-        # Send the maze image after successful generation
-        start_time = time.time()
-        if isinstance(operation, discord.Interaction):
-            if not operation.response.is_done():
-                await operation.response.send_message(file=discord.File(output_file))  # Send initial message
-            else:
-                await operation.followup.send(file=discord.File(output_file))  # Send follow-up if initial response is done
-        else:
-            await operation.reply(file=discord.File(output_file))  # For non-interaction messages
+            start_time = time.time()
+            subprocess.run(["./maze", str(num), output_file], check=True)
+            generation_time = time.time() - start_time
 
-        print("Successfully sent maze image!")
-        print(f"Time to send: \033[96m{round(10*(time.time() - start_time))/10}\033[0m seconds")
-    except subprocess.CalledProcessError as e:
-        error_msg = f"shit went down in the C program: {e}"
-        print(error_msg)
-        if isinstance(operation, discord.Interaction):
-            if not operation.response.is_done():
-                await operation.response.send_message(error_msg)
+            maze_size = os.path.getsize(output_file) / (1024 * 1024)
+            log(f"Maze size: \033[31m{maze_size:.2f}\033[0m MB")
+            log(f"Time to generate: \033[95m{generation_time:.2f}\033[0m seconds")
+            if maze_size > 10:
+                await try_to_send(operation, f"Maze was generated successfully but is too large to send ({round(maze_size)} MB)")
             else:
-                await operation.followup.send(error_msg)
-        else:
-            await operation.reply(error_msg)
-    except Exception as e:
-        error_msg = f"shit went down in the Python program: {e}"
-        print(error_msg)
-        if isinstance(operation, discord.Interaction):
-            if not operation.response.is_done():
-                await operation.response.send_message(error_msg)
-            else:
-                await operation.followup.send(error_msg)
-        else:
-            await operation.channel.send(error_msg)
-
+                await try_to_send(operation, file=output_file)
+                log("Successfully sent maze image!")
+                log(f"Time to send: \033[96m{round(10 * (time.time() - start_time)) / 10}\033[0m seconds")
+        except subprocess.CalledProcessError as e:
+            await try_to_send(operation, "Number was wayyyy too big. told ya so")
+        except Exception as e:
+            log(f"\033[91mShit went down in the \033[93mPython\033[91m program: {e}")
+            await try_to_send(operation, "Too many requests! Try in a few seconds.")
+    else:
+        await try_to_send(operation, "You know full well that that size is too big.")
+        log(f"\033[31muser number was too big\033[0m")
 
 @bot.tree.command(name="maze", description="Generate a maze")
 async def slash_maze(interaction: discord.Interaction, size: str = None):
-    print(f"\n\033[92mslash\033[0m maze request received from \033[94m{interaction.user}\033[0m")
+    log(f"\n\033[96m{datetime.now().strftime("%-m/%-d/%y %H:%M:%S")}\033[0m")
+    log(f"\n\033[92mslash\033[0m maze request received from \033[94m{interaction.user}\033[0m")
     await generate_maze(interaction, separate(size))
 
 @bot.event
@@ -104,7 +89,8 @@ async def on_message(message):
         return
 
     if message.content.startswith('-maze'):
-        print(f"\n\033[93mdash\033[0m maze request received from \033[94m{message.author}\033[0m")
+        log(f"\n\033[96m{datetime.now().strftime("%-m/%-d/%y %H:%M:%S")}\033[0m")
+        log(f"\033[93mdash\033[0m maze request received from \033[94m{message.author}\033[0m")
         await generate_maze(message, separate(message.content.lower())[1:])
 
     await bot.process_commands(message)
@@ -113,5 +99,4 @@ async def on_message(message):
 async def on_ready():
     await bot.tree.sync()
 
-# Run the bot
 bot.run(token)
