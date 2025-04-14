a60517364a79bd09fd83b6ccdac95ef35eb78d15
Tilley <tilleyy8@gmail.com>
2025-04-14 15:14:30 -0600

    Update dailymaze.py

--- dailymaze.py
+++ dailymaze.py
@@ -1,23 +1,28 @@
 from selfbot import send_message, send_file
 from datetime import datetime
-import os, time, random
+import os, time, random, sys
 
-hour = int(input("Hour to send at: "))
-min = int(input("Min to send at: "))
-channel = input("Channel ID to send in: ")
-i = int(input("Number to start at: "))
+if len(sys.argv) != 5:
+    print("python3 dailymaze.py hour min channel_id, start_num")
+    sys.exit(0)
+    
+hour = int(sys.argv[1])
+minute = int(sys.argv[2])
+channel = sys.argv[3]
+i = int(sys.argv[4])
 
 def daily_maze(channel_id):
     global i
     size = random.randint(32,64)
     os.system(f"./maze {size} maze.png")
     send_message(channel_id, f"daily maze #{i}")
+    time.sleep(2)
     send_file(channel_id, "./maze.png")
     i += 1
 
 while True:
     now = datetime.now()
-    if now.hour == hour and now.minute == min:
+    if now.hour == hour and now.minute == minute:
         daily_maze(channel)
         time.sleep(60)
     time.sleep(4)
